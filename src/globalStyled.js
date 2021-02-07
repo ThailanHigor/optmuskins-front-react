a654630d64286ae8ae2db67567a656508c7e1c24
Thailan <thailan-higor@hotmail.com>
2021-02-07 17:30:35 -0300

    font and global styles

---
+++ src/globalStyled.js
@@ -0,0 +1,19 @@
+import styled from 'styled-components'
+
+export const ContentRight = styled.div`
+    padding-left: 380px;
+    box-sizing: border-box;
+    position: relative;
+    margin-top: 0;
+    z-index: 100;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    flex-direction: column
+`;
+
+
+export const Container = styled.div`
+    width: 100%;
+
+`;
