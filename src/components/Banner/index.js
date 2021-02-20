483b7ba71271c3d49245fce5e6457aa3addc8e82
Michell <michell_mrz@hotmail.com>
2021-02-20 16:38:07 -0300

    Banner bottom Home

--- src/components/Banner/index.js
+++ src/components/Banner/index.js
@@ -3,12 +3,7 @@ import './index.css';
 function Banner(){
     return (
         <>
-        <div className="banner__container" style={{backgroundImage: 'url("/assets/images/banner.png")'}}>
-            <div className="banner__info">
-                <h3>Pesquisando skins de CSGO?</h3>
-                <h2 className="banner__h2">Encontramos o melhor preço da sua skin favorita antes do aquecimento acabar</h2>
-            </div>
-        </div>
+        <div className="banner__container" style={{backgroundImage: 'url("/assets/images/banner.png")'}}></div>
         </>
     );
 }
