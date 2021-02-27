e87ea8f5dc94d65b06972fc1ab60c31108de75b3
Michell <michell_mrz@hotmail.com>
2021-02-27 03:09:48 -0300

    Ajustes em banners top e bottom + logo

--- src/components/Banner/index.js
+++ src/components/Banner/index.js
@@ -3,6 +3,7 @@ import './index.css';
 function Banner(){
     return (
         <>
+        <div className="banner__container_mobile" style={{backgroundImage: 'url("/assets/images/banner-top-mobile.png")'}}></div>
         <div className="banner__container" style={{backgroundImage: 'url("/assets/images/banner.png")'}}></div>
         </>
     );
--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -10,6 +10,7 @@ import Banner from '../../components/Banner'
 import Aside from '../../components/Aside'
 import Footer from '../../components/Footer'
 import MoreVisits from '../../components/Home/MoreVisits'
+import Card from '../../components/3dCard'
 
 class Home extends Component {
   render() {
@@ -19,13 +20,13 @@ class Home extends Component {
         <ContentRight>
           <Container className="Home__container">
             <Banner />
-            <MoreVisits  />
-
+            <MoreVisits />
             <div className="banner_mid__container">
-              <div className="banner_mid_content" style={{backgroundImage: 'url("/assets/images/bg-mid.png")'}}>
-                  <a href="/compare-skins">
-                    <span>Compare os preços</span>
-                  </a> 
+              <div className="banner_mid_content_mobile">
+                <Card/>
+              </div>
+              <div className="banner_mid_content" style={{ backgroundImage: 'url("/assets/images/bg-mid.png")' }}>
+                <Card/>
               </div>
             </div>
 
